61b166b4770d2ff20042488243d54c749f9b4f18
João Pedro <devjoaomonteiro@gmail.com>
2025-05-28 14:58:28 -0300

    Docker

--- src/main/java/com/joaopem/itau_challenger_jr/model/Transaction.java
+++ src/main/java/com/joaopem/itau_challenger_jr/model/Transaction.java
@@ -1,13 +1,11 @@
 package com.joaopem.itau_challenger_jr.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.OffsetDateTime;
 
-//@Data
 @Getter
 @Setter
 @AllArgsConstructor
